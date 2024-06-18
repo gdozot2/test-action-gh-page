393798f8faf2f9d0d62ede685dcbb3b6a93a05cd
Grégory Dozot <gregory.dozot@heig-vd.ch>
2024-06-18 09:02:19 +0200

    update build

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: '/test-action-gh-page/'  //you have to add your repo name here 
 })
